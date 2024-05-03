29c78e3ffc8ffd19dbb86270b175aa5fbc44477d
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-03 13:11:37 +0200

    Count

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """ Writing strings to Redis """
 
 from typing import Union, Optional, Callable
+from functools import wraps
 import redis
 import uuid
 
@@ -16,21 +17,16 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-            """ Stores data in the cache and returns the generated key. """
-            key = str(uuid.uuid4())
-            self._redis.set(key, data)
-            return key
-
     def get(self, key: str, fn: Optional[Callable] = None)\
             -> Union[str, bytes, int, float, None]:
-                """ Retrieves data from the cache using the specified key and
-                optionally applies a conversion function.
-                """
-                data = self._redis.get(key)
-                if data is not None and fn is not None:
-                    return fn(data)
-                return data
+        """
+        Retrieves data from the cache using the specified key and
+        optionally applies a conversion function.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
 
     def get_str(self, key: str) -> str:
         """ Retrieves a string value from the cache. """
@@ -39,3 +35,22 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """ Retrieves an integer value from the cache. """
         return self.get(key, int)
+
+    def count_calls(fn: Callable) -> Callable:
+        """
+        Decorator to count the number of times a method of Cache class
+        is called.
+        """
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            key = self.__class__.__qualname__ + '.' + fn.__name__
+            self._redis.incr(key)
+            return fn(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Stores data in the cache and returns the generated key. """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
