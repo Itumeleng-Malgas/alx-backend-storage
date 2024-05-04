90805170879d83fc138828af8291b376a895971d
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-04 02:39:35 +0200

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python3
 """A module with tools for request caching and tracking."""
+
 import redis
 import requests
-from typing import Callable
 from datetime import timedelta
 
 
-def cache_and_track(func: Callable[[str], str]) -> Callable[[str], str]:
-    """Decorator to cache function results and track URL accesses."""
-    def wrapper(url: str) -> str
-        if url is None or len(url.strip()) == 0:
-            return ''
-        redis_store = redis.Redis()
-        res_key = f"result:{url}"
-        req_key = f"count:{url}"
-        result = redis_store.get(res_key)
-        if result is not None:
-            redis_store.incr(req_key)
-            return result.decode("utf-8")
-        result = func(url)
-        redis_store.setex(res_key, timedelta(seconds=10), result)
+def get_page(url: str) -> str:
+    """
+    uses the requests module to obtain the HTML content of a particular
+    URL and returns it
+    """
+    if url is None or len(url.strip()) == 0:
+        return ''
+    redis_store = redis.Redis()
+    res_key = 'result:{}'.format(url)
+    req_key = 'count:{}'.format(url)
+    result = redis_store.get(res_key)
+    if result is not None:
+        redis_store.incr(req_key)
         return result
-    return wrapper
-
-
-@cache_and_track
-def get_page(url: str) -> str
-    """Returns the content of a URL after caching the request's response,
-    and tracking the request."""
-    return requests.get(url).content.decode("utf-8")
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    return result
