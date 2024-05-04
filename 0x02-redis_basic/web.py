a3aed2600b5cf8c5a55f30ef151d35c9f6889e1a
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-04 02:02:11 +0200

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,53 +1,32 @@
 #!/usr/bin/env python3
-"""  Implementing an expiring web cache and tracker """
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
-import time
-from functools import wraps
-from typing import Callable
+import requests
+from datetime import timedelta
 
-
-def cache_with_expiry(expiration: int) -> Callable:
-    """Decorator to cache function results with expiration time"""
-
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            cache_key = f"cache:{func.__name__}:{args}"
-            redis_client = redis.Redis()
-            cached_result = redis_client.get(cache_key)
-            if cached_result:
-                return cached_result.decode("utf-8")
-            result = func(*args, **kwargs)
-            redis_client.setex(cache_key, expiration, result)
+def cache_with_expiry(expiration: int):
+    '''Decorator to cache function results with expiration time'''
+    def decorator(func):
+        def wrapper(url):
+            redis_store = redis.Redis()
+            res_key = f'result:{url}'
+            req_key = f'count:{url}'
+            result = redis_store.get(res_key)
+            if result is not None:
+                redis_store.incr(req_key)
+                return result.decode('utf-8')
+            result = func(url)
+            redis_store.setex(res_key, timedelta(seconds=expiration), result)
             return result
         return wrapper
     return decorator
 
-
-def track_access(func: Callable) -> Callable:
-    """Decorator to track the number of accesses to a URL"""
-
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        redis_client = redis.Redis()
-        count_key = f"count:{url}"
-        count = redis_client.incr(count_key)
-        return func(url)
-    return wrapper
-
-
 @cache_with_expiry(10)
-@track_access
 def get_page(url: str) -> str:
-    """Fetches HTML content from a URL"""
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/"
-    "https://www.example.com"
-    print(get_page(url))
-    time.sleep(5)  # Wait for cache expiry
-    print(get_page(url))  # Cache should still be valid
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    if url is None or not url.strip():
+        return ''
+    return requests.get(url).content.decode('utf-8')
